85a169ce88e38fac105bb8effe415933da558905
Samuel Morais <samuelmorais552@gmail.com>
2025-08-20 10:36:39 -0300

    Initial Commit

---
+++ src/main/java/ufrn/pd/App.java
@@ -0,0 +1,13 @@
+package ufrn.pd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
