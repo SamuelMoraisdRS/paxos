0e5f5025faef38e0057c832755b8d44afc7046bc
Samuel <samuelmorais552@gmail.com>
2025-09-08 11:26:33 -0300

    refactor: Decoupling the Service interface from the low-level message parsing by receiving RequestPayload records rather than Strings

--- src/main/java/ufrn/pd/service/Service.java
+++ src/main/java/ufrn/pd/service/Service.java
@@ -1,8 +1,11 @@
 package ufrn.pd.service;
 
+import ufrn.pd.service.user.dtos.RequestPayload;
+
 /*
 Interface that represent the app's service logic. The handle method will be called the server
  */
 public interface Service {
-    String handle(String request);
+    // THe method to handle a request. In the current implementation, this is a blocking operation
+    RequestPayload handle(RequestPayload request);
 }
