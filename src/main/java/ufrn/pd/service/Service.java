f68991b777824378db5a3b0dd538ba7249480047
Samuel <samuelmorais552@gmail.com>
2025-09-12 09:49:09 -0300

    refactor : Adapt the interfaces of the service layer methods to use ResponsePayload instances when required

--- src/main/java/ufrn/pd/service/Service.java
+++ src/main/java/ufrn/pd/service/Service.java
@@ -1,11 +1,12 @@
 package ufrn.pd.service;
 
-import ufrn.pd.service.user.dtos.RequestPayload;
+import ufrn.pd.service.user.RequestPayload;
+import ufrn.pd.service.user.ResponsePayload;
 
 /*
 Interface that represent the app's service logic. The handle method will be called the server
  */
 public interface Service {
     // THe method to handle a request. In the current implementation, this is a blocking operation
-    RequestPayload handle(RequestPayload request);
+    ResponsePayload handle(RequestPayload request);
 }
