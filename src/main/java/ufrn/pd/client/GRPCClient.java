d0ff7568ebc0f2be7ae5a9438425222648eb74b7
Samuel <samuelmorais552@gmail.com>
2025-09-13 11:11:58 -0300

    fix : Using one Channel per connection

--- src/main/java/ufrn/pd/client/GRPCClient.java
+++ src/main/java/ufrn/pd/client/GRPCClient.java
@@ -4,18 +4,26 @@ import io.grpc.ManagedChannelBuilder;
 import ufrn.pd.gateway.GRPCMapper;
 import ufrn.pd.service.user.RequestPayload;
 import ufrn.pd.service.user.ResponsePayload;
+import ufrn.pd.service.user.protocol.ResponseStatus;
 
 public class GRPCClient implements Client {
     private io.grpc.ManagedChannel channel;
 
     @Override
     public ResponsePayload sendAndReceive(String remoteAddress, int port, RequestPayload message) {
-        if (this.channel == null) {
-            this.channel = ManagedChannelBuilder.forAddress(remoteAddress, port).usePlaintext().build();
-        }
+//        if (this.channel == null) {
+//            this.channel = ManagedChannelBuilder.forAddress(remoteAddress, port).usePlaintext().build();
+//        }
+        io.grpc.ManagedChannel channel = ManagedChannelBuilder.forAddress(remoteAddress, port).usePlaintext().build();
         var stub = projetogrpc.GeneralServiceGrpc.newBlockingStub(channel);
         projetogrpc.Request request = GRPCMapper.toRequestMessage(message);
-        var response = stub.sendRequest(request);
-        return GRPCMapper.toResponsePayload(response);
+        try {
+            var response = stub.sendRequest(request);
+            return GRPCMapper.toResponsePayload(response);
+        } catch (Exception e ) {
+            System.err.println("Exception ao enviar mensagem");
+//            e.printStackTrace();
+            return new ResponsePayload(ResponseStatus.ERROR, "", message.destinationAddress());
+        }
     }
 }
