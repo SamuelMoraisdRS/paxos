285ab4c3bd741ac5e80d3582a7053af975070ca9
Samuel <samuelmorais552@gmail.com>
2025-09-21 14:32:45 -0300

    fix : Use a Map to hold multpile channels on GRPCClient

--- src/main/java/ufrn/pd/client/GRPCClient.java
+++ src/main/java/ufrn/pd/client/GRPCClient.java
@@ -1,28 +1,39 @@
 package ufrn.pd.client;
 
+import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import ufrn.pd.gateway.GRPCMapper;
+import ufrn.pd.gateway.NodeAddress;
 import ufrn.pd.service.user.RequestPayload;
 import ufrn.pd.service.user.ResponsePayload;
 import ufrn.pd.service.user.protocol.ResponseStatus;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class GRPCClient implements Client {
     private io.grpc.ManagedChannel channel;
 
+    private final Map<NodeAddress, ManagedChannel> channelsPerConnection = new ConcurrentHashMap<>();
+
     @Override
     public ResponsePayload sendAndReceive(String remoteAddress, int port, RequestPayload message) {
-//        if (this.channel == null) {
-//            this.channel = ManagedChannelBuilder.forAddress(remoteAddress, port).usePlaintext().build();
-//        }
-        io.grpc.ManagedChannel channel = ManagedChannelBuilder.forAddress(remoteAddress, port).usePlaintext().build();
+        NodeAddress address = new NodeAddress(remoteAddress, port);
+        if (!channelsPerConnection.containsKey(address)) {
+            channelsPerConnection.put(address, ManagedChannelBuilder.forAddress(remoteAddress, port).usePlaintext().build());
+        }
+
+        io.grpc.ManagedChannel channel = channelsPerConnection.get(address);
+
         var stub = projetogrpc.GeneralServiceGrpc.newBlockingStub(channel);
+
         projetogrpc.Request request = GRPCMapper.toRequestMessage(message);
         try {
             var response = stub.sendRequest(request);
             return GRPCMapper.toResponsePayload(response);
         } catch (Exception e ) {
             System.err.println("Exception ao enviar mensagem");
-//            e.printStackTrace();
             return new ResponsePayload(ResponseStatus.ERROR, "", message.destinationAddress());
         }
     }
