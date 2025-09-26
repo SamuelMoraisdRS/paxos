3a76c4397405b6dc249c3ea890ceff5e255fa861
Samuel <samuelmorais552@gmail.com>
2025-09-25 23:51:54 -0300

    fix : Fixed the response creation method on PDProtocol

--- src/main/java/ufrn/pd/utils/protocol/PDProtocol.java
+++ src/main/java/ufrn/pd/utils/protocol/PDProtocol.java
@@ -6,7 +6,6 @@ import ufrn.pd.gateway.NodeAddress;
 import ufrn.pd.gateway.NodeRole;
 import ufrn.pd.service.user.RequestPayload;
 import ufrn.pd.service.user.ResponsePayload;
-import ufrn.pd.service.user.protocol.ResponseStatus;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +17,9 @@ public abstract class PDProtocol implements ApplicationProtocol {
 //    abstract RequestPayload createErrorMessage(List<String> msg);
     @Override
     public  RequestPayload parseRequest(String message) {
-        List<String> msg = List.of(message.split("\n"));
+//        System.out.println("Mensagem recebida : " + message);
+        List<String> msg = List.of(message.split("\r\n"));
+//        System.out.println("Mensagem splitada : " + msg);
         // TODO : Split the validation and error payload creation
         Optional<RequestPayload> errorPayload = validateMessage(message);
         if  (errorPayload.isPresent()) {
@@ -26,13 +27,16 @@ public abstract class PDProtocol implements ApplicationProtocol {
         }
         String destinationAddressLine = msg.get(1);
         // TODO: use substring lookup
-        NodeAddress destinationAddress = NodeAddress.fromString(destinationAddressLine);
+//        System.out.println("DestinationAddressLine: " + destinationAddressLine);
         NodeRole senderRole = NodeRole.valueOf(msg.get(2));
+//        System.out.println("NodeRole : " + msg.get(2));
+        NodeAddress destinationAddress = NodeAddress.fromString(destinationAddressLine);
         NodeRole destinationRole = NodeRole.valueOf(msg.get(3));
         String operation = msg.get(0);
         return new RequestPayload(destinationAddress, senderRole, destinationRole, operation, msg.get(4));
     }
 
+    // Volta
     @Override
     public  ResponsePayload parseResponse(String message) {
         List<String> msg = List.of(message.split("\n"));
@@ -48,13 +52,17 @@ public abstract class PDProtocol implements ApplicationProtocol {
 
     @Override
     public String createRequest(RequestPayload message) {
-        return String.format("%s%n%s%n%s%n%s%n%s%n",message.operation(),
-                message.destinationAddress(), message.senderRole(), message.destinationRole(), message.value());
+        return String.format("%s\r\n%s\r\n%s\r\n%s\r\n%s",
+                message.operation(),
+                message.destinationAddress(),
+                message.senderRole(),
+                message.destinationRole(),
+                message.value());
     }
 
     @Override
     public String createResponse(ResponsePayload message) {
-        return String.format("%s%n%s%n%s%n",message.status().toString(),
+        return String.format("%s\n%s\n%s\n",message.status().toString(),
                 message.senderAddress(), message.value());
     }
 }
